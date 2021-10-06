b4fdd1de6e29ad0247463a9d9c9e6a8828f69822
nishthasri <75921289+nishthasri@users.noreply.github.com>
2021-10-06 17:08:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+function preload() {
+	world_start = loadSound("world_start.wav");
+	jump_m = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+
+video = createCapture(VIDEO);
+video.size(800,400);
+video.parent('game_console');
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+	game()
+	if(noseX < 300){
+marioX = marioX - 1;
+	}
+	if(noseX > 300){
+marioX = marioX + 1;
+	}
+    if(noseY < 150){
+marioY = marioY - 1;
+	}
+	if(noseY < 150){
+marioY = marioY + 1;
+	}
+
+	image(img,marioX,marioY,40,70)
+}
+function modelLoaded(){
+	console.log('Model Loaded!')
+	
+}
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX =" + noseX , "noseY =" + noseY );
+	}
+}
+
+
+
+
+
